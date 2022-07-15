935efe8b0b8ff302c434f927a944dafd6c5d7949
DNParadox <m96francesco@gmail.com>
2022-07-15 15:51:06 +0200

    Second Push

--- src/components/myHeader.vue
+++ src/components/myHeader.vue
@@ -1,16 +1,16 @@
 <template>
     <header>
         <div class="header-top">
-            <div class="container mx-auto">
+            <div class="container">
 
-                <div class="row justify-content-evenly mycontainer align-items-center">
+                <div class="row align-items-center">
 
-                    <div class="col-4 off-set-xl-1"> Open Hours: Mon-Sat - 9:00 - 18:00</div>
-                    <div class="col-2">
+                    <div class="col-4 offset-2"> Open Hours: Mon-Sat - 9:00 - 18:00</div>
+                    <div class="col">
                         <div class="telnumb">+1 (222) 222 333 4444</div>
                     </div>
-                    <div class="col-1"> m96francesco@gmail.com</div>
-                    <div class="col-1 off-set-xl-1">
+                    <div class="col"> m96francesco@gmail.com</div>
+                    <div class="col-2">
                         <!-- Promemoria: Dovrai eseguire ciclo for per ottenere le icone -->
                         <div class="iconbox d-flex">
                             <div class="icon">
@@ -26,14 +26,30 @@
                             </div>
                         </div>
                     </div>
-
                 </div>
 
             </div>
         </div>
 
         <div class="header-bottom">
-           <div class="container"> header bottom</div>
+            <div class="header-shade">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-3 offset-2">
+                            <div class="mylogo">
+                                <span class="halflogo">NEX</span>GEN </div>
+                            </div>
+                        <div class="col"> <a href="#"> Test</a> </div>
+                        <div class="col">test</div>
+                        <div class="col">test</div>
+                        <div class="col">test</div>
+                        <div class="col">test</div>
+                        <div class="col">test</div>
+                        <div class="col">test</div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </header>
 </template>
@@ -45,21 +61,46 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .container {
     height: 100%;
     width: 80%;
 }
-
-.row {
+.header-bottom {
+    .row {
     height: 100%;
+    padding-top: 1.2rem;
+    }
 }
 
 .icon {
     padding-inline: 0.2rem;
 }
 
+header {
+    color: white
+}
+
 .header-bottom {
-    background-image: url();
+    background-image: url(../assets/images/hero-image.png);
+    background-size: cover;
+    height: 500px;
+    background-position-x: center;
+    background-position-y: bottom;
+
+    .header-shade {
+        background-color: rgba(0, 0, 0, 0.7);
+        height: 100%;
+    }
+}
+.mylogo {
+letter-spacing: 3px;
+    .halflogo {
+    border-radius: 13px 1px 1px 13px;
+    padding: 8px;
+    color: rgba(57, 196, 198);
+    background-color: rgba(3, 132, 131, 0.4);
+    padding-left: 20px;
+    }
 }
 </style>
