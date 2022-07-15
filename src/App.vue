25c1dbf36a20cce0b13be9359b8c0c2b4221c5bf
DNParadox <m96francesco@gmail.com>
2022-07-15 19:41:43 +0200

    Finito Header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <MyHeader :dataHeader="this.dataHeader"/>
+    <MyHeader :dataHeader="this.dataHeader" />
   </div>
 </template>
 
@@ -11,38 +11,42 @@ export default {
   name: 'App',
   components: {
     MyHeader
-},
+  },
   data() {
     return {
       dataHeader: {
-          hours: 'Open Hours: Mon-Sat - 9:00 - 18:00',
-          telephone: '+1 (305) 1234-5678',
-          mail: 'hello@example.com',
-          // Icone non funzionante tramite props. Riprovare Lunedì, inserite staticamente
-          icon: { 
-            facebook: '',
-            linkedin: '',
-            twitter: '',
+        hours: 'Open Hours: Mon-Sat - 9:00 - 18:00',
+        telephone: '+1 (305) 1234-5678',
+        mail: 'hello@example.com',
+        // Icone non funzionante tramite props. Riprovare Lunedì, inserite staticamente
+        icon: {
+          facebook: '',
+          linkedin: '',
+          twitter: '',
+        },
+        allNav: [
+
+          {
+            nav: 'HOME',
           },
-          allNav: [
-            {
-              nav: 'HOME',
-            },
-            {
-              nav: 'ABOUT',
-            },
-            {
-              nav: 'SERVICES',
-            },
-            {
-              nav: 'PRICING',
-            },
-             {
-              nav: 'BLOG',
-            },
-          ],
-            
-        
+          {
+            nav: 'ABOUT',
+          },
+          {
+            nav: 'SERVICES',
+          },
+          {
+            nav: 'PRICING',
+          },
+          {
+            nav: 'BLOG',
+          },
+
+        ],
+        subtext: 'Successful strategies require data analysis, creativity and customer focus, taking advantage of the latest technologies to offer excellent services.'
+
+
+
       },
     }
   }
--- src/components/myHeader.vue
+++ src/components/myHeader.vue
@@ -5,7 +5,7 @@
 
                 <div class="row align-items-center">
 
-                    <div class="col-4 offset-2"> <i class="fa-solid fa-clock"></i>  {{ dataHeader.hours }}</div>
+                    <div class="col-5 offset-1"> <i class="fa-solid fa-clock"></i> {{ dataHeader.hours }}</div>
                     <div class="col">
                         <div class="telnumb"> <i class="fa-solid fa-phone"></i> {{ dataHeader.telephone }}</div>
                     </div>
@@ -18,7 +18,7 @@
                             </div>
 
                             <div class="icon">
-                              <i class="fa-brands fa-twitter"></i>
+                                <i class="fa-brands fa-twitter"></i>
                             </div>
 
                             <div class="icon">
@@ -37,36 +37,43 @@
                 <div class="container">
                     <div class="top">
                         <div class="row d-flex">
-                        <div class="col-3 offset-2">
-                            <div class="mylogo">
-                                <span class="halflogo">NEX</span>GEN </div>
+                            <div class="col-4 offset-1">
+                                <div class="mylogo">
+                                    <span class="halflogo">NEX</span>GEN
+                                </div>
                             </div>
-                        <div class="col"> <a href="#" > Test</a> </div>
-                        <div class="col">test</div>
-                        <div class="col">test</div>
-                        <div class="col">test</div>
-                        <div class="col">test</div>
-                        <div class="col">test</div>
-                    </div>
+                            <div class="col" v-for="(element, index) in dataHeader.allNav" :key="index">
+                                <a class="links" href="#"> {{ element.nav }}</a>
+                            </div>
+
+                        </div>
                     </div>
                     <!-- Center side  -->
-                    <div class="bottom d-flex justify-content-center"> 
+                    <div class="bottom d-flex justify-content-center">
                         <div class="row ">
-                            <div class="col"> 
-                              <div class="title"> LEAD &#126; <span class="halftitle"> Customer</span></div>
-                            
+                            <div class="col">
+                                <div class="title"> LEAD &#126; <span class="halftitle"> Customer</span></div>
+
                             </div>
                         </div>
                         <div class="row">
                             <div class="col">
-                                  <div class="subtext">Lorem ipsum d molestiae ratione asperiorpisci impedit dos aut? Error, debitis veniam!</div>
+                                <div class="subtext">{{ dataHeader.subtext }}</div>
 
                             </div>
                         </div>
-                     </div>
+                        <div class="row">
+                            <div class="col">
+                                <div class="boxbutton subtext">
+                                    <div class="btn btn-outline-success mpdi ">   <a href="#">  Get in Touch</a></div>
+                                    <div class="btn btn-outline-success mpdi">   <a href="#">  Get in Touch</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-               
+
             </div>
 
         </div>
@@ -81,7 +88,7 @@ export default {
     props: {
         dataHeader: Object
     }
-    
+
 }
 </script>
 
@@ -89,11 +96,13 @@ export default {
 .top {
     height: 40%;
 }
+
 .header-top {
     .row {
-    height:100%;
+        height: 100%;
+    }
 }
-}
+
 .container {
     height: 100%;
     width: 80%;
@@ -101,7 +110,8 @@ export default {
 
 
 .icon {
-    padding-inline: 0.2rem;
+    padding-inline: 0.6rem;
+    font-size: 0.7rem;
 }
 
 header {
@@ -114,8 +124,9 @@ header {
     height: 600px;
     background-position-x: center;
     background-position-y: bottom;
+
     .row {
-    padding-top: 1.2rem;
+        padding-top: 1.2rem;
     }
 
     .header-shade {
@@ -123,18 +134,20 @@ header {
         height: 100%;
     }
 }
+
 .mylogo {
-letter-spacing: 3px;
+    letter-spacing: 3px;
+
     .halflogo {
-    border-radius: 13px 1px 1px 13px;
-    padding: 8px;
-    color: rgba(57, 196, 198);
-    background-color: rgba(3, 132, 131, 0.4);
-    padding-left: 20px;
+        border-radius: 13px 1px 1px 13px;
+        padding: 8px;
+        color: rgba(57, 196, 198);
+        background-color: rgba(3, 132, 131, 0.4);
+        padding-left: 20px;
     }
 }
 
-.title{
+.title {
     text-align: center;
     font-size: 3rem;
     font-weight: bold;
@@ -142,14 +155,29 @@ letter-spacing: 3px;
 
 .subtext {
     text-align: center;
+    width: 60%;
+    margin: 0 auto;
 }
 
-.halftitle{
+.boxbutton {
+    padding-inline: 0.5rem;
+}
+
+.halftitle {
     padding: 10px;
     background-color: rgba(3, 132, 131, 0.2);
 }
 
 .bottom {
     flex-direction: column;
+}
+
+.links {
+    text-decoration: none;
+    color: white;
+}
+
+.mpdi {
+    margin-inline: 1.5rem;
 }
 </style>
